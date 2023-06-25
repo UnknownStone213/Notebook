29cff89523b7726850e8e595992e4752f296eeab
Vlad <vlad.khvesyukovich@gmail.com>
2023-06-25 13:46:56 +0300

    im trying to add users and db

--- Notebook/Controllers/HomeController.cs
+++ Notebook/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Notebook.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Notebook.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationContext db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
         {
             _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(db.Users.ToListAsync());
         }
 
         public IActionResult Privacy()
